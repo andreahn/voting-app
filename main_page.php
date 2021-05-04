017093d0101e9669664708b39f5fd02ad8d23135
denise v <43755501+denisewest@users.noreply.github.com>
2021-05-04 10:34:24 +0900

    Import CSS file

--- main_page.php
+++ main_page.php
@@ -4,26 +4,35 @@ session_start();
 
 <!DOCTYPE HTML>
 <html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="login-box">
 <?php
 if ($_SESSION["connected"] == 1) { ?>
   Welcome <?php echo $_SESSION["username"]?>!<br>
   <?php
   if ($_SESSION["voted"] == 0) {
     echo "You have not voted yet!";?>a
-    <button onclick="location.href='vote.php'">Vote</button><?php
+    <button class="button" onclick="location.href='vote.php'">Vote</button><?php
   }
   else {
     echo "You have already voted";
   }
   ?><br>
+  <div class="user-box">
   What will you do?<br>
-  <button onclick="location.href='logout.php'">Logout</button>
+  </div>
+  <button class="button" onclick="location.href='logout.php'">Logout</button>
   <?php
 } else {?>
-  Sorry, you are not a valid user, please login!<br>
-  <button onclick="location.href='logout.php'">back</button>
+  <div class="wrong">
+  Sorry, you are not a valid user, please login!
+  </div>
+  <button class="button" onclick="location.href='logout.php'">back</button>
 <?php } ?>
 <br>
-
-
+</div>
+<body>
 </html>
