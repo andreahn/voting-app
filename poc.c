6644d0db1a1ba6a1e84528f3e6ac7374137bfce2
Andrea Nakstad <57245846+andreahn@users.noreply.github.com>
2021-05-05 15:19:28 +0900

    fixed indentation

--- poc.c
+++ poc.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 
 int main () {
-FILE *fp;
-uid_t uid;
-uid = geteuid();
-fp = fopen("poc.txt", "w");
- if(fp == NULL) {
- return 0;
- }
-fprintf(fp, "[Security Beasts PoC]\nOnly www-data can print this without sudo!\nUser id is: %u\nand should be 33", (unsigned)uid);
-fclose(fp);
-return 0;
+  FILE *fp;
+  uid_t uid;
+  uid = geteuid();
+  fp = fopen("poc.txt", "w");
+  if(fp == NULL) {
+    return 0;
+  }
+  fprintf(fp, "[Security Beasts PoC]\nOnly www-data can print this without sudo!\nUser id is: %u\nand should be 33", (unsigned)uid);
+  fclose(fp);
+  return 0;
 }
