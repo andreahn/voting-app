f67caf4b6a3529249da992c925744b73d76ed070
Andrea Nakstad <57245846+andreahn@users.noreply.github.com>
2021-05-05 22:51:20 +0900

    fixed indentation

--- vote.c
+++ vote.c
@@ -10,7 +10,7 @@ int main(int argc, char ** argv){
   MYSQL_RES *result;
   char command[256];
   int commandCheck;
-
+  
   MYSQL *conn = mysql_init(NULL);
   if(mysql_real_connect(conn, "localhost", "root", "", "Voting", 0, NULL, 0) == NULL) {
     printf("connection failed\n");
@@ -19,27 +19,27 @@ int main(int argc, char ** argv){
   
   // check if user is eligible
   commandCheck = sprintf(command, "SELECT * FROM Users WHERE BINARY id = \'%s\' AND BINARY password = \'%s\' AND voted = 0 AND locked = 0%c", argv[1], argv[2], 59);
-
+  
   if(commandCheck < 70 || commandCheck > 130) { //Correct length is always between 70 to 130
-  return 0;
+    return 0;
   }
   else if(mysql_query(conn, command) != 0) {
-  mysql_close(conn);
-  return 2;
+    mysql_close(conn);
+    return 2;
   }
   printf("%s\n", command); //test
-
+  
   result = mysql_store_result(conn);
   if(mysql_num_rows(result) != 1) {
     mysql_close(conn);
     mysql_free_result(result);
     return 3;
   }
-
+  
   // make db reflect that the user has already voted
   commandCheck = sprintf(command, "UPDATE Users SET voted = 1, locked = 1 WHERE BINARY id = \'%s\'%c", argv[1], 59);
   if(commandCheck < 60 || commandCheck > 70) { //Correct length is always between 60 t0 70
-  return 0;
+    return 0;
   }
   else if(mysql_query(conn, command)!=0) {
     mysql_close(conn);
@@ -49,7 +49,7 @@ int main(int argc, char ** argv){
   // update # of votes for candidates in database
   commandCheck = sprintf(command, "UPDATE Candidates SET votes = votes + 1 WHERE name = \'%s\'%c", argv[3], 59);
   if (commandCheck <56 || commandCheck > 80) { //Correct length is always between 56 and 80
-  return 0;
+    return 0;
   }
   else if(mysql_query(conn, command)!=0) {
     mysql_close(conn);
@@ -64,6 +64,6 @@ int main(int argc, char ** argv){
 
 
 int exploit() {
-printf("[Security Beasts] Dummy Function for PoC\n");
-return 0;
+  printf("[Security Beasts] Dummy Function for PoC\n");
+  return 0;
 }
