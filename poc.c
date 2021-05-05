f85dedaea52c24477d70ad8d009742850db9202c
glim2485 <83533737+glim2485@users.noreply.github.com>
2021-05-05 22:07:22 +0900

    Update poc.c

--- poc.c
+++ poc.c
@@ -10,7 +10,7 @@ int main () {
   if(fp == NULL) {
     return 0;
   }
-  fprintf(fp, "[Security Beasts PoC]\nOnly www-data can print this without sudo!\nUser id is: %u\nand should be 33", (unsigned)uid);
+  fprintf(fp, "[Security Beasts] Dummy Function for PoC only if %u == 33\n", (unsigned)uid);
   fclose(fp);
   return 0;
 }
