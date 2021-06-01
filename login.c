e8482c278e72ccaf6b7f55ddb574ebfe19cc2ce1
glim2485 <83533737+glim2485@users.noreply.github.com>
2021-06-01 15:41:22 +0900

    Update login.c
    patched intended vulnerability

--- login.c
+++ login.c
@@ -9,6 +9,14 @@ int main (int argc, char **argv) {
   int commandCheck;
   MYSQL_ROW row;
   MYSQL *conn = mysql_init(NULL);
+  const char special[] = "!@#$%^&*(){}:;<>,.-";
+  char *ret;  
+
+  //check for special characters
+  for(int i = 1; i<3; i++) {
+  ret = strpbrk(argv[1], special);
+  if(ret) {return 0;}
+  }
   
   //connect to database
   if(mysql_real_connect(conn, "localhost", "root", "", "Voting", 0, NULL, 0) == NULL) {
@@ -17,7 +25,7 @@ int main (int argc, char **argv) {
   }
   
   // construct query
-  commandCheck = sprintf(command, "SELECT * FROM Users WHERE BINARY id = \'%s\' AND BINARY password = \'%s\'%c", argv[1], argv[2], 59);
+  commandCheck = snprintf(command, 250, "SELECT * FROM Users WHERE BINARY id = \'%s\' AND BINARY password = \'%s\'%c", argv[1], argv[2], 59);
   
   if(strlen(argv[1]) > 10) { //username is never greater than 10 but unknown for password because hidden
   return 0;
@@ -28,16 +36,15 @@ int main (int argc, char **argv) {
   }
   
   MYSQL_RES *result = mysql_store_result(conn);
-  
-  if(mysql_num_rows(result) != 0) { // successful login
+  if(mysql_num_rows(result) != 0) {
     row = mysql_fetch_row(result);
     printf("%s\n",row[2]);
     mysql_close(conn);
     return 11;
   }
-  else { // failed login
+  else {
     mysql_close(conn);
-    printf(argv[1]);
+    printf("%s", argv[1]);
     printf(" does not exist, or wrong password.\n");
     return 12;
   }
